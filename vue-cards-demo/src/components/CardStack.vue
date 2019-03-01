4cbf7f6c0510de2841400104b885d6114f06101c
John Ingle <jingle1000@gmail.com>
2019-03-01 09:27:08 -0500

    add interact.js libarary

---
+++ vue-cards-demo/src/components/CardStack.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="cards">
+        <game-card
+        v-for="(card, index) in cards"
+        :key="card"
+        :card="card"
+        :is-current="index ===0"></game-card>
+    </div>
+</template>
+<script>
+import GameCard from '@/components/GameCard';
+
+export default {
+    components: {
+        GameCard
+    },
+    props: {
+        cards: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
