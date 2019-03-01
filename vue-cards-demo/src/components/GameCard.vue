e5fefdc158c202533b9ca42a5a983e76a3469115
John Ingle <jingle1000@gmail.com>
2019-03-01 08:10:19 -0500

    add game card component

---
+++ vue-cards-demo/src/components/GameCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card" :class="{isCurrent : isCurrent}">
+        <h3 class="card-title">{{ card.keyWord }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        card: {
+            type: Object,
+            required: true
+        },
+        isCurrent: {
+            type: Boolean,
+            required: true
+        }
+    }
+}
+</script>
